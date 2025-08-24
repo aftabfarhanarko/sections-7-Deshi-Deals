58c4feb8737bdb5ad7710dac6e384a3ceb28b67d
Aftab Farhan <aftabfarhan324@gmail.com>
2025-08-25 05:39:06 +0600

    js file compleat

--- scripet/index.js
+++ scripet/index.js
@@ -1,33 +1,62 @@
-
-
-function call (valu){
+function call(valu) {
   let element = document.getElementById(valu);
-  return element
+  return element;
 }
-
 
 // Event Delegation .children[0].children[0]
 
-document.getElementById('producat-box-sections').addEventListener("click", function (e) {
-    if(e.target.className. includes('btn-all')){
-    //    alert('click This ');
+document
+  .getElementById("producat-box-sections")
+  .addEventListener("click", function (e) {
+    if (e.target.className.includes("btn-all")) {
+      //    alert('click This ');
 
-    // Card Find Img
-    let cardsBtn = e.target;
-    let producatImg = cardsBtn.parentNode.children[0].children[0].src;
+      // Card Find Img
+      let cardsBtn = e.target;
+      let producatImg = cardsBtn.parentNode.children[0].children[0].src;
 
-    // Card Find Producat Name
-    let cardName = e.target;
-    let producatName = cardName.parentNode.children[1].children[1].innerText;
+      // Card Find Producat Name
+      let cardName = e.target;
+      let producatName = cardName.parentNode.children[1].children[1].innerText;
 
+      // Card Find Producat Price
 
-    // Card Find Producat Price
- 
-    let cardPrice = e.target;
-    let producatPrice = cardPrice.parentNode.children[1].children[2].children[0].innerText;
+      let cardPrice = e.target;
+      let producatPrice =cardPrice.parentNode.children[1]
+      .children[2].children[0].innerText;
 
-    console.log(producatImg, producatName,producatPrice);
+      // Total Price
+      let total = document.getElementById("totalprices").innerText;
+
+      let producatPriceTotal = Number(producatPrice) + Number(total);
+
+    //   Find cardes
+       let cardesCointernar = document.getElementById('addes');
+      // Add Cardes
+      let newCard = document.createElement("addes");
+
+      newCard.innerHTML = `
+         <div class="border-1 border-gray-300 flex items-center justify-between py-2 px-2 rounded-lg bg-[#eeebeb] mb-2">
+            <img src="${producatImg}" alt="">
+                <div>
+                  <h2 class="font-bold text-[18px]">${producatName}</h2>
+                  <p class="font-bold text-[18px] text-red-600">${producatPrice}tk</p>
+                </div>                         
+         </div>
+      
+      `;
+   
+      cardesCointernar.appendChild(newCard);
+
+    //   Total Price er  kaj Hoy nai akhno ..?
+      let qunatieyPrice = document.getElementById('totaalsprice').innerText;
+
+      let myBlance = Number(qunatieyPrice) ++;
+     document.getElementById('totaalsprice').innerText = myBlance;
+      
+
+      
     }
-})
+  });
 
-// Test 
+
